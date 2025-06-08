5d7fecb30a8bcb327f05d6ce8147dd3d4998af5f
leedaeho <679748@naver.com>
2025-06-08 18:18:48 +0900

    mailgun init url

---
+++ src/main/kotlin/com/kotlinprc/client/MailGunClient.java
@@ -0,0 +1,14 @@
+package com.kotlinprc.client;
+
+import feign.Response;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@FeignClient(name = "mailgun", url = "https://api.mailgun.net/v3/")
+@Qualifier("mailgun")
+public interface MailGunClient {
+
+    @PostMapping("sandbox30766b81279e44dbb45f11addc523432.mailgun.org/messages")
+    Response sendEmail();
+}
