f8b3004a3006d0acfbc11da7fae4b3c7bc601708
leedaeho <679748@naver.com>
2025-06-09 20:39:49 +0900

    BaseENtity + CUstomer ENtity 추가

---
+++ src/main/kotlin/com/kotlinprc/domain/model/Customer.kt
@@ -0,0 +1,25 @@
+package com.kotlinprc.domain.model
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+
+@Entity
+class Customer : BaseEntity() {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    open var id: Long? = null
+
+    open var email: String? = null
+
+    open var name: String? = null
+
+    open var password: String? = null
+
+    open var birth: String? = null
+
+    open var phone: String? = null
+}
